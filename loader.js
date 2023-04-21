8ba12e7bf3f6542cf298c586a5dd5554d80d260d
rinkusharma75 <rinku.sharma@prodigydataservices.com>
2023-04-21 14:02:43 -0700

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,22 +1,22 @@
-const loader = document.querySelector('.loader');
+const progressBar = document.querySelector('.progress-bar');
 const progress = document.querySelector('.progress');
 let percent = 0;
 
 function progressLoader() {
 	percent += 25;
-	
-	if (percent > 100) {
+
+	if (percent >= 100) {
 		percent = 100;
-		window.location.href = "https://www.example.com"; // Redirect to a different URL upon completing 100%
+		progress.textContent = `Loading... ${percent}%`;
+		progressBar.style.width = `${percent}%`;
+		setTimeout(() => { window.location.href = "https://www.example.com"; }, 500); // Redirect to a different URL upon completing 100%
+		return;
 	}
-	
-	loader.style.borderTopColor = (percent === 100) ? "#00ff00" : "#3498db"; // Change the color of the border when it reaches 100%
-	
+
+	progressBar.style.width = `${percent}%`;
 	progress.textContent = `Loading... ${percent}%`;
-	
-	if (percent < 100) {
-		setTimeout(progressLoader, 1000); // Call the function again every 1 second until it reaches 100%
-	}
+
+	setTimeout(progressLoader, 1000); // Call the function again every 1 second until it reaches 100%
 }
 
 setTimeout(progressLoader, 1000); // Call the function after 1 second
