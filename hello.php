814fe78cddeef465842843de66d579a0b2a2ec2c
rinkusharma75 <rinku.sharma@prodigydataservices.com>
2023-04-21 02:45:07 -0700

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Current Date in PHP</title>
+</head>
+<body>
+	<h1>Current Date in PHP</h1>
+	<?php
+		// get the current date and time
+		$current_date = date('Y-m-d H:i:s');
+		
+		// print the current date and time
+		echo "<p>The current date and time is: $current_date</p>";
+	?>
+</body>
+</html>
