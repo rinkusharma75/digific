7d328fce185c1fcda327f7dfdea76b68d94c9c41
rinkusharma75 <rinku.sharma@prodigydataservices.com>
2023-04-21 13:58:46 -0700

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,26 +1,22 @@
-var progressBar = document.getElementById("progress");
-var statusText = document.getElementById("status");
+const loader = document.querySelector('.loader');
+const progress = document.querySelector('.progress');
+let percent = 0;
 
-var progress = 0;
-var interval = setInterval(function() {
-  if (progress >= 100) {
-    clearInterval(interval);
-    window.location.href = "http://example.com";
-  } else if (progress >= 75) {
-    statusText.innerHTML = "Almost there... " + progress + "%";
-    progress += 25;
-    progressBar.style.width = progress + "%";
-  } else if (progress >= 50) {
-    statusText.innerHTML = "Halfway there... " + progress + "%";
-    progress += 25;
-    progressBar.style.width = progress + "%";
-  } else if (progress >= 25) {
-    statusText.innerHTML = "A quarter done... " + progress + "%";
-    progress += 25;
-    progressBar.style.width = progress + "%";
-  } else {
-    statusText.innerHTML = "Loading... " + progress + "%";
-    progress += 25;
-    progressBar.style.width = progress + "%";
-  }
-}, 1000);
+function progressLoader() {
+	percent += 25;
+	
+	if (percent > 100) {
+		percent = 100;
+		window.location.href = "https://www.example.com"; // Redirect to a different URL upon completing 100%
+	}
+	
+	loader.style.borderTopColor = (percent === 100) ? "#00ff00" : "#3498db"; // Change the color of the border when it reaches 100%
+	
+	progress.textContent = `Loading... ${percent}%`;
+	
+	if (percent < 100) {
+		setTimeout(progressLoader, 1000); // Call the function again every 1 second until it reaches 100%
+	}
+}
+
+setTimeout(progressLoader, 1000); // Call the function after 1 second
